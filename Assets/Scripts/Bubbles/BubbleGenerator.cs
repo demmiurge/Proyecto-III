51b609030f7389afb9fa286482580865bae9d59c
GuillemExe <guillem.arrojo@gmail.com>
2023-04-21 17:23:42 +0200

    Added new input system

---
+++ Assets/Scripts/Bubbles/BubbleGenerator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9bc50def115921983dcc9847975d1a6e3ce67d1d5bc2fedf574291fd4d44468a
+size 677
---
+++ Assets/Scripts/Bubbles/BubbleManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfb6e4bfb95ed2b87aa9cd53a485c21c17311d6c20b0c6fab37f7389e1aff504
+size 2187
