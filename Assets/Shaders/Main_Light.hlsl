718a94fe0a9ae0ad745c900cea93b4079847096e
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-22 01:03:36 +0200

    Water caustics shader

---
+++ Assets/Shaders/Main_Light.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60c780d6aedf4002a98b9fbaff47f10981031cce096a7567be3353d3ae41ce96
+size 785
---
+++ Assets/Shaders/TextureProjection.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9aa45f1d85275f30c70a9f2da6769728c993a5126d2cd5b5a952f8662ce519f
+size 1156
