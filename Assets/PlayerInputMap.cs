f06068be996da7fa5c1feefdecfb1959ac8b904b
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-22 19:48:51 +0200

    Cosas

--- Assets/PlayerInputMap.cs
+++ Assets/PlayerInputMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8540ab513ac3a2b955d60777fe5e06c5ce8b5790f1976f3c10e33cec118fc5b
-size 12641
+oid sha256:a1118b3b84c7ee4d968157c06c7d9a3e45b3f6bb2ad9d489a0387ada7241b1f2
+size 12629
