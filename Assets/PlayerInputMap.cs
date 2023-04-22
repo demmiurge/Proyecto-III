cb38354d73f7d6e0c6feda67eec396f9c9c17f50
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-23 01:05:32 +0200

    Water jump

--- Assets/PlayerInputMap.cs
+++ Assets/PlayerInputMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1118b3b84c7ee4d968157c06c7d9a3e45b3f6bb2ad9d489a0387ada7241b1f2
-size 12629
+oid sha256:e6576ce3f3869515bb02bce19119a006a033c372d491bd7c954e4fe9f842d322
+size 15499
