852c45bda693ed9481d26dff3efc0f2ef0f9ae64
GuillemExe <guillem.arrojo@gmail.com>
2023-04-23 15:00:57 +0200

    Glue in the bubbles script

---
+++ Assets/Scripts/Bubbles/CheckSurface.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0b2d2293e107e135f97616bc774b8f659919e7ffec6646ff7126f723359e986
+size 607
---
+++ Assets/Scripts/Bubbles/GlueBehavior.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:698f3e3f5bf3d5c7129954d70b736a1f55b0a2c5e3a10ce412aa4af4d656beea
+size 1823
