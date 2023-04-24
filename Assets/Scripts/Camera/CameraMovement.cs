17ed8aeb17c966dcdba1f14d5625b789692b1012
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-24 18:28:45 +0200

    Player movement and camera fixed

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8fdb7fc38beeeea5c1988cf83dd335dfb0f45c81f53a3ab19eb0e9bdf6ef9fc
-size 2220
+oid sha256:f45e0e6d472b2b550fe129a920c7041c5b34f43f028037ecc25ce0dd20a44046
+size 2232
