cbbd7c99a5dab7ead8097227eb115df7a7c28a1c
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-24 18:30:29 +0200

    Cosas

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1479f039962cc84613571d77774c88b583644c08cfb4087a0dfedabd6268db82
-size 3698
+oid sha256:4045911e30cb3d36a77fe8c4b54670a6f4061ad3f43ba6c3fd9ba8dbb03470a7
+size 3238
