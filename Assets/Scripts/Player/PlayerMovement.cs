99e092109372effa55c56e47598cfdeed1a15fe0
GuillemExe <guillem.arrojo@gmail.com>
2023-04-27 12:38:48 +0200

    Smoothed shot

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1435a2c27e5deb38e8398184f0323f187d5b009b848bcefb9ac78b89ee9076c5
+oid sha256:108251235cdba04437be3aada1e05229b1916e0d549b9ce2e649e1deb82e4403
 size 9341
