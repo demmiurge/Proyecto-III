2bb6058aeceeb1c00c4e0c444928f2f77a37d541
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-27 18:01:08 +0200

    Gamepad Mapping

--- Assets/PlayerInputMap.cs
+++ Assets/PlayerInputMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28c8efea292d61f00b5bcf61ebb2d78b1ed6ab35107c28572c77713297fb55ed
-size 23137
+oid sha256:babde2456095d12b43c5f1500c4f1a4786b263aeec0f2cb86d7948816677effc
+size 36920
