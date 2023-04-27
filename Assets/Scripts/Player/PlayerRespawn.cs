cac32c11b6f6dc2439de572eb3cb6516839b0cb6
Veronika Bolshakova <veronikabox@gmail.com>
2023-04-27 21:56:49 +0200

    Checkpoints and small fixes

---
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a37f20efb640f270314078adeab96c698bc3e4eba48b5130352851a741bbb81a
+size 1951
---
+++ Assets/Scripts/Utilities/Checkpoint.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:913ac386a5f51842f7f6ba883366ea4f9d2bf019b231ddd66803469a61f50ed3
+size 760
