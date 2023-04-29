4392f682a8e9743b1e62c8d89cdab58b91b6c2da
PolCasas <pcasas@edu.tecnocampus.cat>
2023-04-29 13:34:15 +0200

    PolScene con proto Enemigo Aéreo
    Falta arreglar el movimiento de cuando entra en el estado Attacking, ajustar el fire rate (se queda mirando el objetivo demasiado tiempo), el proyectil hay que redireccionarlo.

---
+++ Assets/Scripts/Enemies/EnemigoAereo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdabf3c6333f642e9e7c800251c932a1f1630fe0834ba572f7cfca8507e6234e
+size 8747
---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a8eeda4960f6313395eecd0036628840732105828e63b5b85b92d63ebb5ec01
+size 625
