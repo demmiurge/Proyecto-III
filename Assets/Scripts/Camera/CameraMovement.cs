8906f97456c0e203d9f7b5f7baef84a775644140
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-05 17:41:53 +0200

    Player things

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec0561be220fb2c0a75778c644573d21bc7b75f2dbeb3b09c42d688cea846ee7
-size 2386
+oid sha256:c56d4b3097f174f76af083ba2e5b8572a2cbe9970a5426b5efe9290f8d909792
+size 2242
