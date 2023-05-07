1c6b0429f988d345dad6ebc5ce6981b9933226fd
GuillemExe <guillem.arrojo@gmail.com>
2023-05-07 17:08:03 +0200

    Platforms and trap done

---
+++ Assets/Scripts/Platforms/RotarySkewers.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8cfa4b0ea8ba10616b5a436baa9a1793cb4b600ece6d863bfe38128ac5f1e246
+size 484
---
+++ Assets/Scripts/Platforms/StickToGround.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a149d6129843e9bd25acfdfa34713a7c3160d4e727914ec3aa6ef8f767b3d90f
+size 873
