2abb664103bf7c62a831eb452f05520f7a6c3878
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-07 21:55:27 +0200

    Input functional

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae5207cf07aaba2dd525e4913023c068e175da6bc470c35bdc000bb2436b4569
-size 8525
+oid sha256:6a7593ce3eb73b35fb531f3732e868ceb6e7f7cbba5c9ddfa93853cfe007f83d
+size 8522
