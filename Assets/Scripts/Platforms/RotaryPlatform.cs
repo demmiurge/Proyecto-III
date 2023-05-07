bbe3eb7a40349f043fe27986bbc0b5cb5db71a56
GuillemExe <guillem.arrojo@gmail.com>
2023-05-07 16:24:43 +0200

    Fixed rotary platform

--- Assets/Scripts/Platforms/RotaryPlatform.cs
+++ Assets/Scripts/Platforms/RotaryPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34a653e6ba2305ed6d0fc722dbaaade3f68781695444ce486256d010a58326b8
-size 1188
+oid sha256:3db2f0661da5bfb06d0fde68c9efb6d5558a52106046e61517ee03f7b61596f8
+size 1170
