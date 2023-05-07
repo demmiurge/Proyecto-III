ec439be55343707e6aec716bcdc8d614dda7e567
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-07 19:30:53 +0200

    progressive movement with gamepad

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83b5d1e222a096ecf9d9395a7d72d56e429784c301bcd5ea3b986540d5e1fab1
-size 8519
+oid sha256:c48b7c27650c5d6d5905cc9db9253429b333aba1c1ec53c4060462574eddccfe
+size 8220
