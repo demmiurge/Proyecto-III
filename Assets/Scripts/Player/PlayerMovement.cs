d2a21c1c3cfdc3e2c8564a0152a339d89cdca84b
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-07 22:01:23 +0200

    Not shooting when in water

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a7593ce3eb73b35fb531f3732e868ceb6e7f7cbba5c9ddfa93853cfe007f83d
-size 8522
+oid sha256:eb977a321e7b8479fbb7431ce0b91532d833dcefbe73ad620c312f129189b418
+size 8598
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0516dc9d4290ceee28a0cbb52f57de8f733e183b6f442ccff146853dc9731a02
-size 3223
+oid sha256:196e309ddc978fd83c206797a0545cd84d951af9b1c0a3148001dff9f4efb294
+size 3339
