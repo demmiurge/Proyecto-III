bd38e8a5829bdba24c466cb156ba80e9072636c8
GuillemExe <guillem.arrojo@gmail.com>
2023-05-07 02:19:42 +0200

    Mobile and rotary platform

---
+++ Assets/Scripts/Platforms/MobilePlatform.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7cb7f1db7766b8a2b7c585865b4da51169a9342af3f8f8aeb4ba3514e6d9ccd
+size 1396
---
+++ Assets/Scripts/Platforms/RotaryPlatform.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:34a653e6ba2305ed6d0fc722dbaaade3f68781695444ce486256d010a58326b8
+size 1188
