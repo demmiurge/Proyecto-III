6c47a971edc4e1010f2f3b243f8bfd64725832f9
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-08 18:00:35 +0200

    Different speed for zoom & control device lost/regained update

--- Assets/Scripts/Player/PlayerInputManage.cs
+++ Assets/Scripts/Player/PlayerInputManage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86ba67478e7196ed27fa23f346023c07aa1b11a29600821294692d34af39e4d5
-size 1282
+oid sha256:7710ec8abb4794a7a835ed217357b4999e40e251a456ddda685d93fb7d1b604a
+size 2614
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9450208e83d3fc2af6a4eebc4971d4635c605d7674a7bcc894c3476a15013f5f
-size 8668
+oid sha256:65415883a01f72549c4af2553f58a722ce9a7a778eafd512e2735462f78f5a7d
+size 9031
