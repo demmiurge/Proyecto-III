95297720c3e731ba6418d79553c582562352b5b2
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-08 20:42:52 +0200

    New jump improved

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe1290703a988adc2f89ba23edf4dca1a854cd1f1ea92cc72961e9290bf1fa7
-size 9459
+oid sha256:f93f5a782b43493cd78ac4f14700dd35409e80d38b42b5c7480d0407db03ab29
+size 9670
