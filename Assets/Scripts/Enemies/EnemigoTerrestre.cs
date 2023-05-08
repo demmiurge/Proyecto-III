3d2359f09d9a8886ebe6d02ca6226988d16b9d76
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-08 20:32:45 +0200

    Enemies finished
    Si al player se le cambia la altura el enemy2 empieza a fallar, la escala tiene que estar a 1. Que me daba errores por no darme cuenta de que estaba cambiada.

--- Assets/Scripts/Enemies/EnemigoTerrestre.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:facc968c79b0caf60ec68da5c7f249e1a24e709971c20f5b0e973c2f12f75a06
-size 8591
---
+++ Assets/Scripts/Enemies/EnemigoTerrestre/EnemigoTerrestre.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f2e983600636e285dc03f04ad68ef3784efc9ccf667336ff14a46e14a4480d5
+size 9545
