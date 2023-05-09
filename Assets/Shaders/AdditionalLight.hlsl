97e51ba6b550a4f989193b5a0fe1375fb77bc84c
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-10 00:29:56 +0200

    Toon shader first stage

---
+++ Assets/Shaders/AdditionalLight.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3af8582d2be8bd3b4cad2e37c8952acd6ae3ef18e1ffaad43122c3d89b2e7ff4
+size 672
---
+++ Assets/Shaders/Lighting.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdef0dc0c46420052231bc4e41c20abb1dbe8e32458f57006af89b657aa272d2
+size 592
