30a748fa5e937e48e78ebf6f0ac0c38bd9df1690
GuillemExe <guillem.arrojo@gmail.com>
2023-05-09 18:27:47 +0200

    Need to fix rotaryPlatform

--- Assets/Scripts/Platforms/RotaryPlatform.cs
+++ Assets/Scripts/Platforms/RotaryPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3db2f0661da5bfb06d0fde68c9efb6d5558a52106046e61517ee03f7b61596f8
+oid sha256:ee32c846ac09ddb239611bf876b565b50b7a319cc3b42e073e244a843a64af04
 size 1170
