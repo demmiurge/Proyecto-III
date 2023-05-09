e37ed3416a7f2d4687cbb3efaef6b5c7eda87802
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-09 19:38:08 +0200

    Prefab update

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e99363e1d155db4c46b7070ee018ae902159e43ed2da7d870d90acb0954730ac
-size 1136
+oid sha256:1c3d4361a783895965291cb39e8edcb0db0f14fd14c857c9636a064e35f75f23
+size 1228
