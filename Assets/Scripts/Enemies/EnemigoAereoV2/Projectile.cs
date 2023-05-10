749af45e23e4c7e66eb40309580dedc9c4578b1c
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-10 18:30:47 +0200

    Pequeño ajuste prefabs enemies.
    (Hay algún que otro proyectil que va donde quiere, se podria hacer alguna logica mejor)

--- Assets/Scripts/Enemies/EnemigoAereoV2/Projectile.cs
+++ Assets/Scripts/Enemies/EnemigoAereoV2/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fd63465239fcdf38ffe2f257950ac4f2b1204b0172f2535d72d2fb337ca518a
-size 1555
+oid sha256:87cae368bccbb1b4cca214f285e42bb8c9acb45140c8a759defc60343d5b354a
+size 1634
