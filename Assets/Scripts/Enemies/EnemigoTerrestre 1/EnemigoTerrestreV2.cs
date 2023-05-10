f45076342dbd32fceceed229f285f5cb6e6ce961
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-10 15:00:08 +0200

    asdfg

--- Assets/Scripts/Enemies/EnemigoTerrestre 1/EnemigoTerrestreV2.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b41911365b2cba88a2096cda835347e789ba36c16870429b4115ef64937eb025
-size 8953
---
+++ Assets/Scripts/Enemies/EnemigoTerrestreV2/EnemigoTerrestreV2.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:913e8cc9c76d21b980a75a1b4ff6ec4e1694428def52d030fe28d4ae8a42dc8b
+size 8814
