286ab2c7c374defcf4a401ce4f1edbe39b3ab8b2
GuillemExe <guillem.arrojo@gmail.com>
2023-05-11 03:38:40 +0200

    Pressure plates and single action mobile platforms

---
+++ Assets/Scripts/Interactive/PressurePlatform.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:238d448e7bbdef47df6e660603ac602f722f2edabc056f1c3d6597f0b0780759
+size 4273
---
+++ Assets/Scripts/Platforms/SingleActivationMobilePlatforms.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d3c26e8c2cc43ec32941ad1589713bd8d02d281c28eb13c5a50bb9405cab3df
+size 2031
