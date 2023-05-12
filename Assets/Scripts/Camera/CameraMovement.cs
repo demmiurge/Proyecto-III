8b51462249faad4bb56cf65ad8db912bc5124602
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-12 18:17:25 +0200

    Player not rotating

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a49ee9c68d0a4b06c74d05a63aa507a349707248783cd7d6a133395ecf6482fe
-size 3283
+oid sha256:890b7e7f629c06087af3b78274b3258dbeec92873ac77b6ea28731b042bc3377
+size 3395
--- Assets/Scripts/Player/PlayerPlatforms.cs
+++ Assets/Scripts/Player/PlayerPlatforms.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c7f0c9af2612fb7a308828ca5b1cea1da518e0f49e5d2c07fca99dfb846e7b8
-size 2404
+oid sha256:756aac582d12264df70b17b579d1a37b3d12edec7b496b1154808cb547e3e167
+size 2490
