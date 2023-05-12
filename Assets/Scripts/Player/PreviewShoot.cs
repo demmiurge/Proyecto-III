e3864bda65ea4b3f0e61505b36413476a6470b31
GuillemExe <guillem.arrojo@gmail.com>
2023-05-12 16:08:12 +0200

    Shot preview

---
+++ Assets/Scripts/Player/PreviewShoot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:40281a6e56498e7f424c7e50345c1bea08b8e3e11f4dc2fa6b7b00f57832c0ef
+size 2056
--- Assets/Scripts/UI/LifeSystemUI.cs
+++ Assets/Scripts/UI/LifeSystemUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17ce1a5d62ee880686d042fc6caa6c00db0fbc83232bd1cba558152919b54c33
-size 2668
+oid sha256:204c82984741a0e35e7b7b0adc6b4777144af7ea5a7f7919e1f68c974bac1045
+size 2436
