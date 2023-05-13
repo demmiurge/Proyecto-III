f866cd22439f07940682fad2cd99fe2b2435cfea
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-13 20:02:01 +0200

    Player bubble parenting problem fix

--- Assets/Scripts/Player/PlayerPlatforms.cs
+++ Assets/Scripts/Player/PlayerPlatforms.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8944271323b1e4df7ed663fb29790070ff7045763e4313d54b673c381cb58618
-size 2550
+oid sha256:e8b8e577dbfb3260da758c0314ab0fa05aa4b572caf6004e4363adb7ae04a658
+size 2559
