9b1ecae28462bf466b4a94f07d331a502c723731
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-13 13:26:31 +0200

    Parametrized FOV

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4bc11492fcafd0b8e595241e07f65fd94ece4c96ebccc7c73d0c9bbbc50a1fe
-size 10370
+oid sha256:c66a7adbd8d12c1d15b6ed19a4858a95ca449e957a51b89d3a83107814d64591
+size 10475
