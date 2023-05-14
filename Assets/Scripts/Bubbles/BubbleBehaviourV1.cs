e5f68cee8473bcc7f0c3cae12dcaecaab92ad890
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 17:02:43 +0200

    Fix PowerTower

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b65c989a6bfed3ec5f7dd885e33a27d424a9e30b575ba93b5d629f78aa2abb1
-size 10884
+oid sha256:4667c9ddf85108ba705bc8f89bc7de1d94938a4b863a9ff666749aae343e8416
+size 9794
--- Assets/Scripts/Bubbles/ElectricityGenerator.cs
+++ Assets/Scripts/Bubbles/ElectricityGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6745037ded93d77241e024f43a0fe11795f234e66fd39401034d35b8672ed72
-size 1519
+oid sha256:04fd8359b7d9aad1cd2ad3924ec12064b8fa39b0288776d45bdc9bd21fd6cfc1
+size 1683
