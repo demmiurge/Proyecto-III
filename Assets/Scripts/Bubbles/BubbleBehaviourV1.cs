e62c5740652f8bfbeba73df3e7638a73a2abbfd3
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 15:26:36 +0200

    Undock player when destroying bubble

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba86b179f35258c733ee358428153ea8bdba7fdff8d56ca103e8ce8b684244a
-size 10500
+oid sha256:9866fe3ef3b4b6ae08b0070ba6428a25af8d98a168e725dbaeb82387ad79f9ad
+size 10818
