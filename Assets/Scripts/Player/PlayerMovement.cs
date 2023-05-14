5b6c0270dead72df6c3eb089a1d1fa48bed7ebb7
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-14 18:12:13 +0200

    Water jump fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bac770ded1d5791f01546403bc20ad9877ead76d6ddb717fac6334b5c68a95e
-size 11718
+oid sha256:a935d29165b6c340f92326b5bdb17d74cfe68291a7b92d5118c71d853fd71dac
+size 11727
