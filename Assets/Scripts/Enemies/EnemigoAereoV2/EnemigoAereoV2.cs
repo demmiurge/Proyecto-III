d526e2ec8d663c51cee9127d4d85786ccb44e8e6
Animorama <jmora.aparicio@gmail.com>
2023-05-14 19:47:57 +0200

    Fix: Enemigo Aéreo (Funcional)

--- Assets/Scripts/Enemies/EnemigoAereoV2/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereoV2/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3feb3cfff540625dece68b8da624604c4e368adff8d378eaab03ed1c4112bf3
-size 13835
+oid sha256:757cd8f6a54184755ce7e55773e8db141fcea224dd4196422c9e7eac871da825
+size 14270
--- Assets/Scripts/Enemies/EnemigoAereoV2/Projectile.cs
+++ Assets/Scripts/Enemies/EnemigoAereoV2/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6d568dec6409bbab9c4bbe3c691aeb285d4d96a82ea86a2808d49c673ba012b
-size 2226
+oid sha256:64a75bbc9118e5200b4df2bfe713790bfc17a4b29507df2f3e584dfbec4c3772
+size 2322
