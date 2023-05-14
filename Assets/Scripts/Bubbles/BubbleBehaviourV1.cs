00f6ad13e1bd68e3053fc904d3bff28fcef7a611
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 12:02:18 +0200

    Bubbles can destroy

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ae14543f05d2606802bd5cb91d3942dc0cbab06dd97c9d86a2e09b299d84492
-size 9916
+oid sha256:e4b637430854c851c6aed6f39f24ef540681ff8bd36f62628f79974f9cc9a478
+size 9913
