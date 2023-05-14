5eb7dea9cf8484008c6486ee43eb9467237df7e7
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 03:05:21 +0200

    Fixed shoot

--- Assets/Scripts/Player/PreviewShoot.cs
+++ Assets/Scripts/Player/PreviewShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3efc8ada018892acd900de6cd2efe964da6b53b967ccb7caf4d884c3d32788d9
-size 5221
+oid sha256:f4be4d661be6d0b9bc04c3685cbe4c1fa0f3e9eb29b98dc6ce8a7330419b7e19
+size 5380
