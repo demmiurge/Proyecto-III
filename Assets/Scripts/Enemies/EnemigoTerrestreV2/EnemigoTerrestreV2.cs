232000e54724d3ad3b31348f93ad4b15cf70a177
Animorama <jmora.aparicio@gmail.com>
2023-05-14 17:58:19 +0200

    WIP Fix: Enemigo Terrestre (Stun Recuperado)

--- Assets/Scripts/Enemies/EnemigoTerrestreV2/EnemigoTerrestreV2.cs
+++ Assets/Scripts/Enemies/EnemigoTerrestreV2/EnemigoTerrestreV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9ae0df3388042bc21783afdebacb049ea28f0c3d5d66c8c99fa821a95b80678
-size 10111
+oid sha256:4aee9fc61445cc829c1500c07280dff762e12b0014ac8a0bab663026d66ded16
+size 10200
