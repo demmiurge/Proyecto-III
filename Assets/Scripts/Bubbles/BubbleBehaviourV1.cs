fa2b46b48c18d2c0aba98bdfbbf20f3a47d46976
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 20:46:03 +0200

    Fix fucking bubble

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4667c9ddf85108ba705bc8f89bc7de1d94938a4b863a9ff666749aae343e8416
-size 9794
+oid sha256:45c102d3d203be7a1c4217b8ec336867f69b8fea4b7cf55e2939d396566ff82c
+size 9879
--- Assets/Scripts/Player/PlayerPlatforms.cs
+++ Assets/Scripts/Player/PlayerPlatforms.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8b8e577dbfb3260da758c0314ab0fa05aa4b572caf6004e4363adb7ae04a658
-size 2559
+oid sha256:8a0737ad3f3df1575c6656a9f441928cd9a524ea6202b432c4e75cd0e37f5eb3
+size 2651
