b1c28ee944345800dedb3fc104ac59b72a0c7519
GuillemExe <guillem.arrojo@gmail.com>
2023-05-14 12:13:37 +0200

    Bubbles have a kill timer

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4b637430854c851c6aed6f39f24ef540681ff8bd36f62628f79974f9cc9a478
-size 9913
+oid sha256:92555f025bbdff77c39dbc14ebe3b48d6f0ad6b79c3dd30523bac3a8bab18d37
+size 10246
