98c17a97d8c54202d2e89f7ebcbe1e8a46993cf2
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-16 17:49:45 +0200

    Color changing shadow in toon shader

---
+++ Assets/Shaders/Water/HLSL/NoiseUtils.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:47cb30bd54fe52ed8ca7daf2fb4156d651f0016f41f6af12e14a4a6491b7df41
+size 2859
---
+++ Assets/Shaders/Water/HLSL/Triplanar.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83ac54b0484ae1d603b1af7e3bdd5e706a8e77e471a0f34ac2e6376776636b3a
+size 5156
