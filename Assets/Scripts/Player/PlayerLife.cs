b4693d39eec120b0cd96dbde013792b220fe3dad
GuillemExe <guillem.arrojo@gmail.com>
2023-05-17 21:51:17 +0200

    First take ProdScene

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3299ff615b65b60b2aaa0d58629ebf445bd96e4f1ff3926d6f8a71ede2b0181d
-size 1495
+oid sha256:00a6dd80203d77ea4d09dd0c73096d4e681875ae75e53854a9efe2e6811f960b
+size 1606
