5a211cef1a9f586f928578f0869bdf2b243f5161
GuillemExe <guillem.arrojo@gmail.com>
2023-05-17 16:41:58 +0200

    Fixing prodScene

--- Assets/Scripts/Bubbles/BubbleBullet.cs
+++ Assets/Scripts/Bubbles/BubbleBullet.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c53c77d1d3f73e9cb4da115b98316d76bd6f074abdf37705b9ac6bf05e551745
-size 3017
+oid sha256:35b556dd0a360098951a1aaaf3a3a7d2eddd449375f7e5b0eb2a9fa3e4c9516e
+size 3015
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:075fcd1c346dcb6ef92d2ce2412ec9b868cdd8c1b5f1fd820d9b5cd9765561b7
-size 3061
+oid sha256:bfdbb7e113c46dac2290c6eb6477cfb28fe809ca5db1c70364e3383df8ae3f61
+size 3062
