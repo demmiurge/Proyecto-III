1b8d09c3c6244b9a06c088f5b75fbed4b438dad9
GuillemExe <guillem.arrojo@gmail.com>
2023-05-17 02:09:21 +0200

    Creating core managers

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d57e3117c8397187bbadd242c65544f1082f3deb13148a4fab8ce0afc6fb5fe
+size 867
---
+++ Assets/Scripts/Core/PauseController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:955c4a40b0bda642dc481034d87767d62363504a8cfdb339cc864ff2b44ec27b
+size 468
