3411cfe1739e538dc81f27bfdce7ed57fe419d1e
Animorama <jmora.aparicio@gmail.com>
2023-05-19 20:39:43 +0200

    New Player Prefab

--- Assets/Scripts/Player/PlayerInputManage.cs
+++ Assets/Scripts/Player/PlayerInputManage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94dc75492f90713b6659aa36de14f9350b9f07b9678ec40b6ef41df353ea3ae3
-size 3161
+oid sha256:5abbeda85811014bf6f2da91313cc8a53656f7f24c8abc28fe361ed07100a533
+size 3162
