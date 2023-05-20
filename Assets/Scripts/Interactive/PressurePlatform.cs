b52a25af6d903cd5f7361c86faab92e204271202
GuillemExe <guillem.arrojo@gmail.com>
2023-05-20 02:38:47 +0200

    Improved performance pressure plate

--- Assets/Scripts/Interactive/PressurePlatform.cs
+++ Assets/Scripts/Interactive/PressurePlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30c66afd9b14e10210cac0d97343964b6d1c92b98e0e563123e3fdc3cd1f5caf
-size 4104
+oid sha256:bdbea587e5e951bd5b8f135b9e36197ba6009e0f7dcdd74627ad73a74219226a
+size 4840
