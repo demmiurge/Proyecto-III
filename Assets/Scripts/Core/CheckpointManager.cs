2f5d0cf534b56e1e5b0512a49850221f51702329
GuillemExe <guillem.arrojo@gmail.com>
2023-05-23 00:12:58 +0200

    Restart live already works

---
+++ Assets/Scripts/Core/CheckpointManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86c071865314b8f47c049d1cc93840c038f7c89f8b997d95582c3edd231f8638
+size 1478
--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a6dd80203d77ea4d09dd0c73096d4e681875ae75e53854a9efe2e6811f960b
-size 1606
+oid sha256:abb320dd42549b1282cc34f414b05407da8c02a3ef5687b7314db8cfecb4b6c5
+size 1639
