901b5a299a0b71b3a64a5b0267026110e0113662
GuillemExe <guillem.arrojo@gmail.com>
2023-05-22 18:11:55 +0200

    Prefab BubbleBullet updated collision detection

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14b7ad66cd9d10eab0763d43a4f4ef3e4a2e31a533ed927925f122f885a7c8c
-size 4692
+oid sha256:5fdfb126b1da5496a3a06f1b585a8060a519c1724449a8d169a7e93270d94b0a
+size 4584
