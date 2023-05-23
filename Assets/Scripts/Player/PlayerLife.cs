3a2421eec31c90772548248a1b80996766ef7815
GuillemExe <guillem.arrojo@gmail.com>
2023-05-23 11:25:20 +0200

    Player can heal

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abb320dd42549b1282cc34f414b05407da8c02a3ef5687b7314db8cfecb4b6c5
-size 1639
+oid sha256:05cefbe6fead5bd23809a35a03bd681ca4f9983ef7e5267c96c6bb447e3380ce
+size 1958
