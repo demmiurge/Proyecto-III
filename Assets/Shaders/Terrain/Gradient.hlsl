1bc545ce1b4b21561104cb4fae421d209b0fda34
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-23 23:52:21 +0200

    Sand shader

---
+++ Assets/Shaders/Terrain/Gradient.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcd5103b161fbda0b9e461290b2caf7c4f557adf2503d674ac72d850afc0a932
+size 493
---
+++ Assets/Shaders/Terrain/NoiseSand.hlsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c8df786f909005b253cc6597df1028ca36a0f534db413e5722c539c18b4b34b
+size 7538
