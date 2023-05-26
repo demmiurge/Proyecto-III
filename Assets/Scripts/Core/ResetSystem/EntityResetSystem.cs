151b9102de4190979ccde6403c3f03b94fb60611
GuillemExe <guillem.arrojo@gmail.com>
2023-05-26 18:51:38 +0200

    Prepare to add fmod

---
+++ Assets/Scripts/Core/ResetSystem/EntityResetSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7111b47ddcb8b635fefb38d0a850a6bf91ff6f6bd2c197f04c2b97476b8bac56
+size 457
--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e6d08c1e1e5c932113c8e59b899d4a8f14129a1faadac72cffb74c0bcef48ff
-size 5051
+oid sha256:08c761ccb07790cb2670c3e103e1239a5f4e6230abfe3876066e8f2a9cfbd430
+size 5050
