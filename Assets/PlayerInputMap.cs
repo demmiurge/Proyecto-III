8d0ef5bfeff482e5b702eaef57e788480db1b99d
AlexRodriguez <77335677+darkbeez08@users.noreply.github.com>
2023-05-27 00:19:21 +0200

    he cambiado el input del alt al espacio en keyboard

--- Assets/PlayerInputMap.cs
+++ Assets/PlayerInputMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74447185c3ede3387bfb132a1cb4f9967bbbb9e3f80f5ad4590b5c0369c11411
-size 76567
+oid sha256:2dd002ec3e3b3adeeb1367608e7df875e75f6a193bc75348385446472db69777
+size 76565
