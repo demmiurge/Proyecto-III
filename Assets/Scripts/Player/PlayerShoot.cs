879f3f73b5f675d03d844cc330e8feae3cb585b0
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-27 10:52:36 +0200

    Shooting animations & decorative water shader update

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36259a2ada835b5960a6a638f47cb61c0658e9046283f3bfa277cd3fe2b17125
-size 3262
+oid sha256:811d4263617ab315ac9c0b4aeabc28235da5a463265dee79ced86de193e2a732
+size 3285
