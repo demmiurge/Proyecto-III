567090de02f55518e7ab9723f86795eeb851ce79
GuillemExe <guillem.arrojo@gmail.com>
2023-05-28 21:07:10 +0200

    Fix bubble plus size

--- Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
+++ Assets/Scripts/Bubbles/BubbleBehaviourV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18e12c8ebc170886ef5ad1a94e756c5e74428364755ab27833d754e1c90dbf76
-size 9870
+oid sha256:4c639ee23e001443b234071e72c164644ec633aefeaeeb32f53b8e7fa031a781
+size 10511
