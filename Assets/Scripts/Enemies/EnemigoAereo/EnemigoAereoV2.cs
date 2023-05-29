4de37ce0f84cd173ccae89a9e3cbd46c0a16719f
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-29 20:57:54 +0200

    Logica Aereo

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75b6f3b2e47b7dbb28b5433665e55e21c8cdfb3652ec2cb468a1436c3da932cc
-size 15816
+oid sha256:6aa6a30be539256d583245964dc1707259b2cadd6fc02b3448db468ef6a0a5a4
+size 16059
