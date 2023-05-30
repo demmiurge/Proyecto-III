11f2e5ff8eba935ace2b4e25bfb95f0c30112640
GuillemExe <guillem.arrojo@gmail.com>
2023-05-31 00:26:44 +0200

    Fix double shot

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a2b4d0b99e3efe8d6ae7e744ad50c4d29f95e5ea9147f1f39d12d68700540e8
-size 17096
+oid sha256:e50adfa0ada31510cb5d191977b6ba67b1727fa6c430455aa6fe30dd414a37c2
+size 17320
