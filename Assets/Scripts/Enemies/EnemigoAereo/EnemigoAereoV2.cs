ba62866cabba6e809ca39661f3e5d9b4a3a1f9f3
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-30 19:30:16 +0200

    JADkhsad

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aa6a30be539256d583245964dc1707259b2cadd6fc02b3448db468ef6a0a5a4
-size 16059
+oid sha256:b596224498e44ba3d0359f8f6b54e3ea5cf13da1bc457590dc8beba067f2b9bd
+size 16897
