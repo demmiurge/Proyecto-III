b8f5431a8ca3f4084cb26de6aed26d5e1ae7eeb3
Veronika Bolshakova <veronikabox@gmail.com>
2023-05-30 17:15:06 +0200

    Soy gilipollas

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d56702f871752dee4e14e3a5fe4ada6ed074a197862964552f3895029ee60866
-size 15029
+oid sha256:8637ae9d47af331208ea2ec7d45fa5f5f8e42bb1b95e973d29b8845db2c887a0
+size 14868
