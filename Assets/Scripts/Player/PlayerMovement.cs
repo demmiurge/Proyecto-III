111382c32e8dd53ed84b2ad8fa15d4653ae8dc37
GuillemExe <guillem.arrojo@gmail.com>
2023-05-31 15:21:31 +0200

    Fixed super jump collisions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f51c1e93fa36fe70be1c4f34fa1cec179f974d88025af6a7e052f0f1b0207e2
-size 16013
+oid sha256:5bfa5727e397b99bb35e5a5e5977a50373d99fdfa98a211a855acb32eb041e6a
+size 16321
