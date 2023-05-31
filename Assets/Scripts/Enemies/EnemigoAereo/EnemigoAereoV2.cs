c26ca3d9e4b4cc6cdbd4d20c646664f4267dd8f3
PolCasas <pcasas@edu.tecnocampus.cat>
2023-05-31 02:02:06 +0200

    asdfgh

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e50adfa0ada31510cb5d191977b6ba67b1727fa6c430455aa6fe30dd414a37c2
-size 17320
+oid sha256:d65877fed0e4b5fc54d54155290a4c080acce34cf06308c58c0494334ea2dee5
+size 17172
