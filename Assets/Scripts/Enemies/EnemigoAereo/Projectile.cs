e2912d24581579ae9f210848dc5a4cf0d3491825
Animorama <jmora.aparicio@gmail.com>
2023-06-01 16:18:49 +0200

    Small Updates to NavMeshAsset

--- Assets/Scripts/Enemies/EnemigoAereo/Projectile.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c77778237fb608578ff20a103523c2323cc4ad6bcaf0090ed47639f00d6a34a9
-size 1882
+oid sha256:5be523a49910cca7813f66f2d6f4b5ef03e62043d2a194eef5efc3b036dfed52
+size 1880
