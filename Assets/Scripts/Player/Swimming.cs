732dfcebb36be0bdd5f333f5c05d9dd92395c261
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-01 15:15:16 +0200

    Particles destroy fix

--- Assets/Scripts/Player/Swimming.cs
+++ Assets/Scripts/Player/Swimming.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:852c104fccdf39a03451e24021f0ec2d03d3be23d581f1cf3541f12606300c35
-size 5092
+oid sha256:0bd84ab5bf5d114e740f130733d1a86d5d5d524dea969cf45b8efbd2d4c59064
+size 5103
