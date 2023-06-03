010d441b88ded4d2aa5f6ce22fa12ec19d85900a
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-03 22:58:54 +0200

    Camera walls fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c716691885937aaf88db103279eaaa7acbb5c4f3d2a5af183f1ba112e9abc211
+oid sha256:04ebf101bd1f41f5c2c5a2b253e6cb4995a515ba85f8cabd35dead75da351a5d
 size 17520
