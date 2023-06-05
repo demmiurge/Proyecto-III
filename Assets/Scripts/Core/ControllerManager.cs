fec63c62054092605044107705bb53176817e341
GuillemExe <guillem.arrojo@gmail.com>
2023-06-05 18:30:40 +0200

    Fixing menu keyboard and mouse

--- Assets/Scripts/Core/ControllerManager.cs
+++ Assets/Scripts/Core/ControllerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f83a2727d89f2ec0836342a45b3dc6cb106f6e796d941970a25a81fd69c2b2f9
-size 622
+oid sha256:38ae46e1e5336b798874529a63487c4a151cf767118445ae8c8fa483a53c106f
+size 524
