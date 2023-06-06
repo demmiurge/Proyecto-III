e9356e8377946240dadee6bdec4e1e9140f8bd30
GuillemExe <guillem.arrojo@gmail.com>
2023-06-06 13:57:06 +0200

    Bubbles reset when player dies

--- Assets/Scripts/Bubbles/BubbleManagerV1.cs
+++ Assets/Scripts/Bubbles/BubbleManagerV1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c8cd15658a1dd9c83f12f5e93d602ab4b59806ed69efbe0bab9d860c7160abe
-size 5062
+oid sha256:bfe27ae66e64aac07592c16f39cf3353c8f817c2a583ef3ab1dee6d5f7e7c8d1
+size 5241
