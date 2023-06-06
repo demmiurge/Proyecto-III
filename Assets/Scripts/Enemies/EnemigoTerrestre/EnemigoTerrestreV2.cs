bc894a7404622652c48046bda3f087adad013e60
PolCasas <pcasas@edu.tecnocampus.cat>
2023-06-06 17:22:30 +0200

    save prefabs

--- Assets/Scripts/Enemies/EnemigoTerrestre/EnemigoTerrestreV2.cs
+++ Assets/Scripts/Enemies/EnemigoTerrestre/EnemigoTerrestreV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40c1914a175ab1c102bd24f3482c3544eb3139bfbf7036980c222755031790c9
-size 12029
+oid sha256:b3881e94bcef05d09611c5caaee5848c6ebc9a0e17808a4a937e4c9c94b26eff
+size 12026
