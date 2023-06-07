1f85be81291a0939de01306e66a6aed5f3cb7e22
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-08 01:32:11 +0200

    Player prefab update

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65614ede2b61aeb4ba8d00c849762381b20c50d86a72a5ca86dca646601d3356
-size 21283
+oid sha256:d55cc98da205c9c60714142589e65c38de10effa2c4191c4dab897987a95ef23
+size 21486
