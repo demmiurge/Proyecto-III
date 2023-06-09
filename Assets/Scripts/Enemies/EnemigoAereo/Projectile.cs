dad475624bf29d5fbdff410e604c2c143832aafc
PolCasas <pcasas@edu.tecnocampus.cat>
2023-06-09 19:54:46 +0200

    Trail + FootSteps enemigos

--- Assets/Scripts/Enemies/EnemigoAereo/Projectile.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4429d7c1b1125b8427f97a03ace9273dee670afc3b335b94d9f87c3ec415fc4b
-size 2370
+oid sha256:58ce84d4081cea847865b7b5d068cf7ded2386e212606fc3c15b090c69c6153e
+size 2368
