e67160672363a6fb624b9c059deaea0d3c932e4e
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-10 12:23:29 +0200

    Player fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5bbbef7eb4cc8acf32d8e23da96b77b1b641bcafc4a820a132d59d46f9c1707
-size 22460
+oid sha256:153d109381423871660cfa11d8e4cfd727469a97e3074b606768daec17237a2b
+size 22438
