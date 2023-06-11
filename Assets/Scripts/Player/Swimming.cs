bc7f1de589f084d577f74673431038c8c5e366b9
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-11 19:23:32 +0200

    Maybe water anim fix?

--- Assets/Scripts/Player/Swimming.cs
+++ Assets/Scripts/Player/Swimming.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26463dceabe91100e58c40db2d8bf5e55a296a592a9a4a695ada361e80a65e40
-size 5066
+oid sha256:26e14477f112a0c1d4125e25ee109fcc8710f908b2a9a7d798e6e496216b444f
+size 5100
