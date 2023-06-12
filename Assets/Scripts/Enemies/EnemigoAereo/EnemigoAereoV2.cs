11ad5a2b29c5e7c03e6263226ebada691801deb7
PolCasas <pcasas@edu.tecnocampus.cat>
2023-06-13 00:40:13 +0200

    Shado adjust new map

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e0e8e731be8fee70e72be01d567fd862c0022b9bac08b60743e3d2c2fd1db72
-size 20130
+oid sha256:de18942b363ac7dceedefc14429336fff63cbf356dc90d12b0035e4181d34745
+size 20605
