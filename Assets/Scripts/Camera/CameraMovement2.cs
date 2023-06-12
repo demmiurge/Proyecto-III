a42838af9fb65fe7137b56833b7b962faf95f929
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-12 17:49:29 +0200

    Camera zoom fix

--- Assets/Scripts/Camera/CameraMovement2.cs
+++ Assets/Scripts/Camera/CameraMovement2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9deb947bcd7d7ef3145c5d2c9f2305012f249f0d835319f2caae65414cb7df9
-size 4149
+oid sha256:1b49791dd03a53fedca1ae05791a0e4fc6e61a4deb088693feb20c2bfb31be1f
+size 4186
