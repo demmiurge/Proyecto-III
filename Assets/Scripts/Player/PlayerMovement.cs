57e503a910c21a53e31ee5872e886b98431058cc
Veronika Bolshakova <veronikabox@gmail.com>
2023-06-12 17:46:13 +0200

    Jump run fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d2ebbd856ba7dea2db22c5e636f5b53ecd7972df94aaaef7fbf1a8e5a739a0
-size 23353
+oid sha256:825adfe11a4c2c5d550e8ecc8090449ca54956302290408831f960e7d47a45af
+size 23623
