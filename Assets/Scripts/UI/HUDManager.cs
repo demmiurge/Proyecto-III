20b4ab54254d1455b5818516dec25ababd3f9f50
GuillemExe <guillem.arrojo@gmail.com>
2023-06-13 04:16:52 +0200

    Haciendo UI me voy a dormir

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5924a638a9d1c765634bdbf22beb74f852eeffe8a984188f364eba9915bda44d
-size 5288
+oid sha256:2717ed652492596aa25db3755b373154991cb054e800b802a5b7f909c7f7f708
+size 5543
