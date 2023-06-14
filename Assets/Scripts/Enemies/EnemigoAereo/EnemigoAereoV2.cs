6294b58450fa253107aa5741066afc814e8e92c3
PolCasas <pcasas@edu.tecnocampus.cat>
2023-06-15 00:45:10 +0200

    Sounds enemigos por proximidad (ajustable)

--- Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
+++ Assets/Scripts/Enemies/EnemigoAereo/EnemigoAereoV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c38072fee755bf0895121e19606b8b18fc7d860695dedca06a73310688cb16a1
-size 20985
+oid sha256:6cba4f08d94689af545cc95ebead8dd0ca470c95f0901bf03519f2f1ad66198b
+size 22223
