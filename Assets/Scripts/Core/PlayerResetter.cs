837fd738d1ebf1b44ff5f0c9cec97ff0e7956271
GuillemExe <guillem.arrojo@gmail.com>
2023-06-14 14:57:33 +0200

    Improved add score UI

--- Assets/Scripts/Core/PlayerResetter.cs
+++ Assets/Scripts/Core/PlayerResetter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf5cacd3a2f801a5ae6504cebbcd43c6976a66300172c3471dc65ad67550f032
-size 4498
+oid sha256:7bf3b56b8743124f4e0466555a61b4ff35b67a749435dacabccb60c5b958c2a5
+size 4721
--- Assets/Scripts/UI/TotalScoreUI.cs
+++ Assets/Scripts/UI/TotalScoreUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c41b88464c8e3d19a2b1258d3f4c8d720d13be9347a89218a82fc1c06e983281
-size 846
+oid sha256:9e938acfee0c6ad6588e63473ce7ea206d8f21b1a5144b4614bc4a722ae24d8e
+size 1056
