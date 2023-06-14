aa51d1c4d6cf72e0219a93c18723989a8c7df8e4
GuillemExe <guillem.arrojo@gmail.com>
2023-06-14 20:43:36 +0200

    Fixing

--- Assets/Scripts/Platforms/SingleActivationMobilePlatforms.cs
+++ Assets/Scripts/Platforms/SingleActivationMobilePlatforms.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2797323461967ddb4d4f83b6b1d5224b5263e2a743dcb415bc218241f2e39029
-size 1841
+oid sha256:44ffb0c02839a2bcc26b2bc85ae795d11398dfab5cbd57b1b0c31d9c6e597270
+size 1906
