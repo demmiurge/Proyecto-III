b49464c8cbaad323ac6b3198525fb43e198655d3
GuillemExe <guillem.arrojo@gmail.com>
2023-06-15 03:17:21 +0200

    Game loop finished

---
+++ Assets/Scripts/Core/EventListenerCore.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:358b1682c156a9233ac52d2404bcaabeb1b6bbf403b29b3e7efb59c79faf7aa9
+size 407
--- Assets/Scripts/Core/GameFinisher.cs
+++ Assets/Scripts/Core/GameFinisher.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49aa3d2dee28e99c8493401d32dcf75f4c8ec3da9d81162687fe5bdd63931b20
-size 1138
+oid sha256:1af980691779cac1e98384b7240d91800877663c77db165fc2f7763a30895da0
+size 1201
