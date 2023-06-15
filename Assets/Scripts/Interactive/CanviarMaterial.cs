958489ade2bef695055566df632dac20a6e8fd0a
GuillemExe <guillem.arrojo@gmail.com>
2023-06-15 21:49:10 +0200

    Fixing

---
+++ Assets/Scripts/Interactive/CanviarMaterial.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f1696d47105c723f92b0bcaf3fa56b8da0ed6e7fe1a6419ae074ad1d5dc29d9
+size 325
---
+++ Assets/Scripts/Interactive/ChangeMaterial.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97865aaefe48317fe136012c892fd4ea9aacf514fbe185bd22af442dd102a4ab
+size 814
